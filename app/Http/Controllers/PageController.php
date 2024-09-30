9187546ddbddbbdf07f2d6a4ab62b5d9985cdb14
Joserra <j.parada.vigo@gmail.com>
2024-09-30 12:57:44 +0200

    ALMACENAR cambios SEMANA

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Obra;
+use App\Models\Plantilla;
+use App\Models\Provincia;
+use App\Models\Cabreplanteo;
 use Illuminate\Http\Request;
+use App\Models\Emplazamiento;
+use App\Models\CuatroplusTransmision;
+use App\Models\Cuatroplus_transmision;
+
 
 class PageController extends Controller
 {
@@ -27,7 +34,30 @@ class PageController extends Controller
         $post = $slug;
         return view('post',['post' => $post]);
     }
-    public function obras(){
+    public function obras(Request $request){
+        if(isset($request->id)){
+            dd($request->id);
+            return Obra::find($request->id);
+        }
         return Obra::get();
+    }
+
+    public function provincias(){
+        return Provincia::get();
+    }
+
+    public function emplazamientos(){
+        return Emplazamiento::get();
+    }
+
+    public function cabreplanteos(){
+        return Cabreplanteo::get();
+    }
+    public function plantillas(){
+        return Plantilla::get();
+    }
+
+    public function cuatroplusTransmisiones() {
+        return CuatroplusTransmision::get();
     }
 }
